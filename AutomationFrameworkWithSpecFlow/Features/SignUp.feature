4a9b397581da302d2860d65a544eac8cb2a49b65
Pheonix Jones <pjones@agmonitoring.com>
2023-03-09 10:55:48 -0700

    Added step definitions file

---
+++ AutomationFrameworkWithSpecFlow/Features/SignUp.feature
@@ -0,0 +1,12 @@
+﻿Feature: SignUp
+
+A short summary of the feature
+
+@tag1
+Scenario: Sign Up
+	Given I navigate to the website
+	And I enter username
+	And I enter password
+	And I enter email
+	When I click the submit button
+	Then I should be able to sign up and sign in
---
+++ AutomationFrameworkWithSpecFlow/StepDefinition/SignUpStepDefinitions.cs
@@ -0,0 +1,45 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace AutomationFrameworkWithSpecFlow.StepDefinition
+{
+    [Binding]
+    public class SignUpStepDefinitions
+    {
+        [Given(@"I navigate to the website")]
+        public void GivenINavigateToTheWebsite()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I enter username")]
+        public void GivenIEnterUsername()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I enter password")]
+        public void GivenIEnterPassword()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I enter email")]
+        public void GivenIEnterEmail()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click the submit button")]
+        public void WhenIClickTheSubmitButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should be able to sign up and sign in")]
+        public void ThenIShouldBeAbleToSignUpAndSignIn()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
