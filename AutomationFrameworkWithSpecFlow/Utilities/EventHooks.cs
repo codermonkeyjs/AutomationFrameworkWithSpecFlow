706e82b44c2c9358f79c75a7e3bbdb8c64e83baa
Pheonix Jones <pjones@agmonitoring.com>
2023-03-09 11:04:23 -0700

    Created before and after scenarios for starting chrome driver and ending it

---
+++ AutomationFrameworkWithSpecFlow/Utilities/EventHooks.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace AutomationFrameworkWithSpecFlow.Utilities
+{
+    [Binding]
+    public class EventHooks
+    {
+        public static IWebDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+    }
+}
